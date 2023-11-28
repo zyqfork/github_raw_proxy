3e7936fc0cb029537e41ba2cc0f3ad170be06430
zouyq <zyqcn@live.com>
2023-11-28 11:54:34 +0800

    test

--- main.ts
+++ main.ts
@@ -2,13 +2,11 @@ import { serve } from "https://deno.land/std@0.150.0/http/server.ts";
 import { CSS, render } from "https://deno.land/x/gfm@0.1.22/mod.ts";
 
 async function handleRequest(request: Request) {
-  const { pathname, searchParams } = new URL(request.url);
   const { hostname, pathname, searchParams } = new URL(request.url);
   // 去掉第一个字符
   const modifiedHostname = hostname.slice(1);
   const url = new URL(pathname, modifiedHostname);
 
-  const url = new URL(pathname, modifiedHostname);
   const token = searchParams.get("token");
   if (token) {
     url.searchParams.delete("token");
